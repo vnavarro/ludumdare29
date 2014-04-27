2ee0c924b53fbc2393411e256ea38c87af03593c
Vitor Navarro <vitor.navarro87@gmail.com>
2014-04-27 16:10:42 -0300

    Load initial assets

--- js/main.js
+++ js/main.js
@@ -3,7 +3,20 @@ var game = new Phaser.Game(
     { preload: preload, create: create, update: update }
   );
 
+var platforms;
+var key_items;
+var player;
+var score = 0;
+var scoreText;
+
 function preload() {
+  game.load.image('sky', 'assets/sky.png');
+  game.load.image('wall', 'assets/platform160.png');
+  game.load.image('wall_up', 'assets/platform160_I.png');
+  game.load.image('star', 'assets/star.png');
+  game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+
+  cursors = game.input.keyboard.createCursorKeys();
 }
 
 function create() {
